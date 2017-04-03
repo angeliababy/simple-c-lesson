27661200ba874cd22084aa74217b9c3e941eb094
weihongwei <hwwei@thoughtworks.com>
2017-04-03 13:53:21 +0800

    Initial Commit

---
+++ simple-list/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  simple-list
+//
+//  Created by Hongwei
+Wei on 2017/4/3.
+//  Copyright © 2017年 Hongwei
+Wei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
